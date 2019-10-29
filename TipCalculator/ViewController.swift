f8646f9c75bb9f932986694cf9fbb47c9c055b4c
James Wu <ywu@lcs.on.ca>
2019-10-29 14:34:56 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by James Wu on 2019-10-29.
+//  Copyright © 2019 James Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
