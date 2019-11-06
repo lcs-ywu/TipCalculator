2242f73ff45c53222f12a043fbc3c4affd34c108
James Wu <ywu@lcs.on.ca>
2019-11-06 15:08:16 -0500

    Currently getting the input by force unwrapping optional values.

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   
+    // MARK: Properties
+    // Outlets, variables, or constants that we want to use anywhere in methods below
     @IBOutlet weak var amountOfBill: UITextField!
     @IBOutlet weak var tipPercentage: UITextField!
     @IBOutlet weak var splitBetwwenHowManyPeople: UITextField!
+    
+    @IBOutlet weak var outputTipAmount: UILabel!
+    
+    @IBOutlet weak var outputTipPerPerson: UILabel!
+    
+    // MARK: Methods
+    // Actions, to make something happen
+    
+    // Runs when the view appears for the first time
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // 1. Get each input value as a string
+        let incomeAsString = amountOfBill.text!
+        
+        // 2. Convert values to Double
+        let incomeAsDouble = Double(incomeAsString)!
+        
+        // 3. Calculate the tip
+        
+        // 4. Report the results
+        
+        
+    }
+    
+    
 }
 
